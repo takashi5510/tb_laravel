29c3fef109f109ddd1481f92c68f764255e29d7b
Takashi Ishii <takashi5510@gmail.com>
2018-10-01 19:12:15 +0900

    投稿データの保存を実装

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,15 +5,24 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     //
-	public function edit()
-	{
-		return view('admin.profile.edit');
-	}
-	public function update()
-	{
-		return redirect('admin/profile/edit');
-	}
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+    public function update(Request $request)
+    {
+        $this->validate($request, Profile::$rules);
+        $profile = new Profile;
+        $form = $request->all();
+
+        unset($form['_token']);
+        $profile->fill($form);
+        $profile->save();
+        return redirect('admin/profile/edit');
+    }
 }
