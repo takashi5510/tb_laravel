e3265e4d7fb32c5bef094722ea04759ce2a4163a
Takashi Ishii <takashi5510@gmail.com>
2018-11-07 18:44:08 +0900

    heroku deploy2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,10 +14,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-    }
 
-    if (\App::environment('production')) {
-        \URL::forceScheme('https');
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
     /**
      * Register any application services.
